23deda8d760acf5ef8207c174a6fd29995baff98
mybloger <929523071@qq.com>
2021-04-28 11:13:03 +0800

    Site updated: 2021-04-28 11:13:02

---
+++ dist/music.js
@@ -0,0 +1,19 @@
+onst ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '9420',
+        artist: '麦小兜',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      }
+    ]
+});
