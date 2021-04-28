fa77538d2802b53e48aae4ecd486e5c129848c57
mybloger <929523071@qq.com>
2021-04-28 13:34:36 +0800

    Site updated: 2021-04-28 13:34:36

--- dist/music.js
+++ dist/music.js
@@ -1,19 +1,20 @@
-onst ap = new APlayer({
+const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        name: "保留",
+        artist: '黄义达',
+        url: 'http://music.163.com/song/media/outer/url?id=26084247.mp3',
+        cover: 'http://p4.music.126.net/91x8fsHx6_9wzrlqtcK6qA==/2485995790424429.jpg',
+        lrc: '保留-黄义达.lrc'
       },
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+        name: 'Ship In The Sand',
+        artist: 'Marble Sounds',
+        url: 'http://music.163.com/song/media/outer/url?id=26511658.mp3',
+        cover: 'http://p4.music.126.net/mRPJzDzQlELlfQ6qSlnItA==/4428832836684306.jpg',
       }
     ]
 });
