846e174b288a5cf66aedd48e4ede6348601e4ae9
mybloger <929523071@qq.com>
2021-04-28 15:35:13 +0800

    Site updated: 2021-04-28 15:35:12

--- dist/music.js
+++ dist/music.js
@@ -1,6 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
+    lrcType: 3,
     autoplay: false,
     audio: [
       {
@@ -8,13 +9,14 @@ const ap = new APlayer({
         artist: '黄义达',
         url: 'http://music.163.com/song/media/outer/url?id=26084247.mp3',
         cover: 'http://p4.music.126.net/91x8fsHx6_9wzrlqtcK6qA==/2485995790424429.jpg',
-        lrc: '保留-黄义达.lrc'
+        lrc: 'images/保留-黄义达.lrc'
       },
       {
         name: 'Ship In The Sand',
         artist: 'Marble Sounds',
         url: 'http://music.163.com/song/media/outer/url?id=26511658.mp3',
         cover: 'http://p4.music.126.net/mRPJzDzQlELlfQ6qSlnItA==/4428832836684306.jpg',
+        lrc: 'images/Ship In The Sand-Marble Sounds.lrc'
       }
     ]
 });
